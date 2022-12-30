5959526bf72ef09eb50751d52a3d3b80a917704c
rabie <r.raad@xpay.de>
2022-12-30 12:29:00 +0100

    fix: missing path on tw config

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,31 +3,30 @@ import styles from "./index.module.scss";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto flex max-w-md flex-col items-center justify-center text-white">
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by
-        <span>
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            width={72}
-            height={16}
-            className="m-10"
-          />
-        </span>
-      </a>
-      <svg
-        className=" mt-8 h-6 w-6 fill-sky-100 stroke-sky-500 stroke-2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <circle cx="12" cy="12" r="11" />
-        <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-      </svg>
+    <footer className="mx-auto flex max-w-md flex-col items-center justify-center gap-8 text-white sm:flex-row sm:gap-16">
+      <div>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black"
+        >
+          Powered by
+          <span>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </div>
+      <div>
+        <svg
+          className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="11" />
+          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+        </svg>
+      </div>
 
       <div className={styles.description}>something</div>
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "class", // 'media' or 'class'
   theme: {
     extend: {},
   },
