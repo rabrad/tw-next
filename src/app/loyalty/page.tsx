4f46873dcbafedf74abef239ea72ace9f727855c
rabie <r.raad@xpay.de>
2023-01-02 23:55:42 +0100

    use arbitrary variance values

--- src/app/loyalty/page.tsx
+++ src/app/loyalty/page.tsx
@@ -6,16 +6,12 @@ export default function page() {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden  ">
       <div className=" flex h-full max-w-7xl  flex-1 flex-col justify-center gap-2 self-auto py-4 px-4 sm:flex-row sm:gap-8 sm:px-20 sm:py-10">
-        <aside className=" flex h-fit min-w-[150px] flex-col gap-y-4 pb-8 text-gray-800 ">
-          <div className="cursor-pointer border-b border-gray-800 px-8 pb-2 hover:border-teal-600 hover:text-teal-600 hover:duration-300 hover:ease-in-out">
-            <Link href="#" className="">
-              Link 1
-            </Link>
+        <aside className=" flex h-fit min-w-[150px] flex-col gap-y-4 pb-8 [&>*]:cursor-pointer [&>*]:border-b [&>*]:border-gray-800 [&>*]:pb-2  [&>*]:text-gray-800">
+          <div className=" px-8 hover:border-teal-600 hover:text-teal-600 hover:duration-300 hover:ease-in-out">
+            <Link href="#">Link 1</Link>
           </div>
-          <div className="cursor-pointer border-b border-gray-800 px-8 pb-2 hover:border-teal-600 hover:text-teal-600 hover:duration-300 hover:ease-in-out">
-            <Link href="#" className="">
-              Link 2
-            </Link>
+          <div className=" px-8 hover:border-teal-600 hover:text-teal-600 hover:duration-300 hover:ease-in-out">
+            <Link href="#">Link 2</Link>
           </div>
         </aside>
         <main>
